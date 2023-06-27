f3960cb7585174474d20792a5155a3692cb994eb
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-06-27 10:30:51 +0530

    code init, theme setup, Daisyui

---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '	1536px'
+    },
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      primary: '#3e4861',
+      secondary: '#0c2a54',
+      lightgray: '#f8f9fb',
+      lightgreen: '#e5ebe9',
+      lightviolet: '#e8e5ea',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          black: '#000000',
+          white: '#FFFFFF',
+          primary: '#3e4861',
+          secondary: '#0c2a54',
+          lightgray: '#f8f9fb',
+          lightgreen: '#e5ebe9',
+          lightviolet: '#e8e5ea',
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
+}
