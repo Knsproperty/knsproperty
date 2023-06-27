3ea4114cc217438e8d648b995d4a9e0441749c24
dag-bag <virenderkumar23435@gmail.com>
2023-06-27 07:50:54 -0700

    Filters

--- src/blocks/atoms/filterbar/index.tsx
+++ src/blocks/atoms/filterbar/index.tsx
@@ -1,60 +1,98 @@
+import React, { useState } from "react";
+import { beds_options, price_min_max, property_types } from "./data";
 
 const Filterbar = () => {
-    return (
-        <div className="bg-white w-full mt-5 absolute p-5 border-2 rounded-3xl">
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [propertyType, setPropertyType] = useState("");
+  const [bedrooms, setBedrooms] = useState("");
 
-            <div className="grid-cols-2 grid gap-5">
-
-                <div>
-                    <h4 className="mb-3">Minimum Price</h4>
-                    <Select />
-                </div>
-
-                <div>
-                    <h4 className="mb-3">Maximum Price</h4>
-                    <Select />
-                </div>
-
-            </div>
-
-            <div className="grid-cols-2 grid gap-5 mt-5">
-
-                <div>
-                    <h4 className="mb-3">Property Type</h4>
-                    <Select />
-                </div>
-
-                <div>
-                    <h4 className="mb-3">Bedrooms</h4>
-                    <Select />
-                </div>
-
-            </div>
-
-
-            <div className="mt-5 flex justify-end">
-                <button className="py-3.5 px-20 text-white text-sm bg-primary rounded-full ">Search</button>
-            </div>
-
-
-
+  return (
+    <div className="bg-white w-full mt-5 absolute p-5 border-2 rounded-3xl">
+      <div className="grid-cols-2 grid gap-5">
+        <div>
+          <h4 className="mb-3">Minimum Price</h4>
+          <Select
+            data={price_min_max}
+            value={minPrice}
+            onChange={setMinPrice}
+            disabledOption="Minimum Price"
+          />
         </div>
-    )
-}
 
-export default Filterbar
+        <div>
+          <h4 className="mb-3">Maximum Price</h4>
+          <Select
+            data={price_min_max}
+            value={maxPrice}
+            onChange={setMaxPrice}
+            disabledOption="Maximum Price"
+          />
+        </div>
+      </div>
 
+      <div className="grid-cols-2 grid gap-5 mt-5">
+        <div>
+          <h4 className="mb-3">Property Type</h4>
+          <Select
+            data={property_types}
+            value={propertyType}
+            onChange={setPropertyType}
+            disabledOption="Property Type"
+          />
+        </div>
 
+        <div>
+          <h4 className="mb-3">Bedrooms</h4>
+          <Select
+            data={beds_options}
+            value={bedrooms}
+            onChange={setBedrooms}
+            disabledOption="Bedrooms"
+          />
+        </div>
+      </div>
 
-const Select = () => {
-    return (
-        <select className="select w-full max-w-xs border border-primary rounded-full">
-            <option disabled selected>Price Min</option>
-            <option>AED 75,000 PA</option>
-            <option>AED 75,000 PA</option>
-            <option>AED 75,000 PA</option>
-            <option>AED 75,000 PA</option>
-            <option>AED 75,000 PA</option>
-        </select>
-    )
-}
+      <div className="mt-5 flex justify-end">
+        <button className="py-3.5 px-20 text-white text-sm bg-primary rounded-full">
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Filterbar;
+
+const Select = ({
+  data,
+  value,
+  onChange,
+  disabledOption,
+}: {
+  data: string[];
+  value: string;
+  onChange: (value: string) => void;
+  disabledOption: string;
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    onChange(event.target.value);
+  };
+
+  return (
+    <select
+      className="select w-full max-w-xs  border-primary  p-2.5 text-gray-500 bg-white border rounded-full shadow-sm outline-none appearance-none focus:border-indigo-600"
+      value={value}
+      onChange={handleChange}
+    >
+      <option disabled value="">
+        {disabledOption}
+      </option>
+      {data.map((item, index) => (
+        <option key={index} value={item}>
+          {item}
+        </option>
+      ))}
+    </select>
+  );
+};
