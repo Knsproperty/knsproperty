b95e7345dfdc0534419f0ce93b9b121f39f73a7a
dag-bag <virenderkumar23435@gmail.com>
2023-06-30 02:53:50 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,22 @@ import { Search } from "@/blocks/sections";
 import Trendings from "@/blocks/sections/trending";
 
 import Book_Valuation from "@/blocks/sections/book-valuation";
-export default function Home() {
-  // const data = await getData();
-  // console.log(data);
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
 
   return (
     <main>
-      {/* {JSON.stringify(data)} */}
+      {JSON.stringify(data)}
       <Search />
       <Trendings />
       <Book_Valuation />
     </main>
   );
 }
-// async function getData() {
-//   let buy_properties = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/buy`, {
-//     cache: "force-cache",
-//   });
-//   return await buy_properties.json();
-// }
+async function getData() {
+  let buy_properties = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/buy`, {
+    cache: "force-cache",
+  });
+  return await buy_properties.json();
+}
