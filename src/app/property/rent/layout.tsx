e2189328cbbb20eb326fdcc7e0ead3a72fbd30e8
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-02 18:57:05 +0530

    Update layout.tsx

--- src/app/property/rent/layout.tsx
+++ src/app/property/rent/layout.tsx
@@ -1,9 +1,11 @@
 interface Props {
     children: any
 }
+import Filter from "@/blocks/molecules/filter"
 import Exclusive from "@/blocks/sections/exclusive"
 const Layout: React.FC<Props> = ({ children }) => (
     <>
+        <Filter />
         <Exclusive type="rent" />
         {children}
     </>
