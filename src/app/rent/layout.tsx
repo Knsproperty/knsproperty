33373c45aaf015599b1f89edfaa10f0ca10e4d38
dag-bag <virenderkumar23435@gmail.com>
2023-07-02 06:45:49 -0700

    Layout Update

---
+++ src/app/rent/layout.tsx
@@ -0,0 +1,13 @@
+interface Props {
+  children: any;
+}
+import Filter from "@/blocks/molecules/filter";
+import Exclusive from "@/blocks/sections/exclusive";
+const Layout: React.FC<Props> = ({ children }) => (
+  <>
+    <Filter />
+    <Exclusive type="rent" />
+    {children}
+  </>
+);
+export default Layout;
