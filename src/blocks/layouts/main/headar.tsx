ab7d0553f4922ac68faf88f7ff18b1d764c724d5
dag-bag <virenderkumar23435@gmail.com>
2023-07-13 02:01:19 -0700

    Update headar.tsx

--- src/blocks/layouts/main/headar.tsx
+++ src/blocks/layouts/main/headar.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link"
+import Link from "next/link";
 import Logo from "@/blocks/atoms/logo";
 import { FiPhone } from "react-icons/fi";
 import Drawer from "@/blocks/atoms/drawer";
@@ -12,65 +12,84 @@ const Headar = () => {
         <Container>
           <div className=" h-[70px] grid lg:grid-cols-[1fr_2fr_1fr] grid-cols-[1fr_1fr] ">
             <div className="_center">
-              <Link href={'/'}><Logo /></Link>
+              <Link href={"/"}>
+                <Logo />
+              </Link>
             </div>
 
             <nav className="xl:flex items-center justify-around ml-5 hidden">
-              {__meta.map((link, index) => link.options
-                ? <HeaderDropdown
-                  key={index}
-                  href={link.href}
-                  text={link.text}
-                  options={link.options} />
-                : <Link
-                  key={index}
-                  href={link.href}
-                  className="text-sm font-light hover:underline capitalize">{link.text}</Link>
+              {__meta.map((link, index) =>
+                link.options ? (
+                  <HeaderDropdown
+                    key={index}
+                    href={link.href}
+                    text={link.text}
+                    options={link.options}
+                  />
+                ) : (
+                  <Link
+                    key={index}
+                    href={link.href}
+                    className="text-sm font-light hover:underline capitalize"
+                  >
+                    {link.text}
+                  </Link>
+                )
               )}
             </nav>
             <Drawer />
 
             <div className="_center hidden lg:flex">
-              <button className="bg-primary py-4 text-white hover:bg-white group hover:border hover:border-primary hover:text-primary font-medium text-sm rounded-full capitalize px-8 flex gap-2 items-center"><FiPhone color="white" className="stroke-white group-hover:stroke-primary" />Contact Us</button>
+              <button className="bg-primary py-4 text-white hover:bg-white group hover:border hover:border-primary hover:text-primary font-medium text-sm rounded-full capitalize px-8 flex gap-2 items-center">
+                <FiPhone
+                  color="white"
+                  className="stroke-white group-hover:stroke-primary"
+                />
+                Contact Us
+              </button>
             </div>
-
           </div>
         </Container>
-      </header >
+      </header>
 
       <div className="h-[70px]">&nbsp;</div>
-
     </>
-  )
-}
+  );
+};
 
-export default Headar
+export default Headar;
 
 const __meta = [
   {
-    text: 'buy', href: '/property/buy', options: [
-      { text: 'apartment for sale', href: "/about-us/careers" },
-      { text: 'villas for sale', href: "/about-us/team" },
-      { text: 'townhouses for sale', href: "/" },
-      { text: 'commercial for sale', href: "/contact" },
-    ]
+    text: "buy",
+    href: "/property/buy",
+    options: [
+      { text: "apartment for sale", href: "/property/buy?type=AP" },
+      { text: "villas for sale", href: "/property/buy?type=VH" },
+      { text: "townhouses for sale", href: "/property/buy?type=TH" },
+      { text: "commercial for sale", href: "/property/buy?type=OF" },
+    ],
   },
   {
-    text: 'rent', href: '/property/rent', options: [
-      { text: 'apartment for rent', href: "/about-us/careers" },
-      { text: 'villas for rent', href: "/about-us/team" },
-      { text: 'townhouses for rent', href: "/" },
-      { text: 'commercial for rent', href: "/contact" },
-    ]
+    text: "rent",
+    href: "/property/rent",
+    options: [
+      { text: "apartment for rent", href: "/property/rent?type=AP" },
+      { text: "villas for rent", href: "/property/rent?type=VH" },
+      { text: "townhouses for rent", href: "/property/rent?type=TH" },
+      { text: "commercial for rent", href: "/property/rent?type=OF" },
+    ],
   },
-  { text: 'commercial', href: '/' },
-  { text: 'property management', href: '/' },
-  { text: 'off plan', href: '/' },
+  { text: "commercial", href: "/" },
+  { text: "property management", href: "/" },
+  { text: "off plan", href: "/" },
   {
-    text: 'explore more', href: '/', options: [
-      { text: 'careers', href: "/about-us/careers" },
-      { text: 'our team', href: "/about-us/team" },
-      { text: 'contact us', href: "/contact" },
-    ]
+    text: "explore more",
+    href: "/",
+    options: [
+      { text: "careers", href: "/about-us/careers" },
+      { text: "our team", href: "/about-us/team" },
+      { text: "contact us", href: "/contact" },
+    ],
   },
-]
+];
