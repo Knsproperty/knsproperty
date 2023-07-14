93b38cf7e13ebcad700ba3a3c22329ada63bbb44
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-14 12:00:51 +0530

    fix

--- src/blocks/sections/trending.tsx
+++ src/blocks/sections/trending.tsx
@@ -22,7 +22,7 @@ const Trendings: React.FC<Props> = async ({ title, type }) => {
           </div>
 
           <div className="2xl:max-w-[1320px] xl:max-w-[1320px] max-w-[100vw]">
-            <Slider length={data.length}>
+            <Slider length={data.length ?? 0}>
               {data.map(({ attributes }: any, index: number) => (
                 <PropertyCard key={index} {...{
                   bedroom: attributes.Bedrooms,
