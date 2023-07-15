1cb22cc91e437a9690a4f018dd9b5122b6a7448a
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-15 14:06:14 +0530

    template create and implemented

--- src/app/property/rent/[slug]/page.tsx
+++ src/app/property/rent/[slug]/page.tsx
@@ -1,55 +1,24 @@
-import React from "react";
-import Container from "@/blocks/atoms/container";
-import Image from "@/blocks/atoms/image-preview";
-import Formetter from "@/blocks/atoms/text-formatter";
-import ContactCard from "@/blocks/molecules/cards/contact";
-import addCommasToNumber from "@/app/lib/addCommasToNumbers";
-import LocationCard from "@/blocks/molecules/cards/location";
-import { LuBedDouble, LuBath, LuMaximize } from "react-icons/lu";
-
+import Template from "@/blocks/templates/detailed";
 export default async function page({ params }: any) {
   const { slug } = params;
   const [{ attributes }] = await getProperty(slug);
   return (
-    <Container>
-      <div>
-        <Image media={[...attributes.Cron_Images.data.map((e: any) => e.url)]} />
-
-        <section className="grid lg:grid-cols-[3fr_1fr] gap-10 lg:px-0 px-5">
-          <main>
-            <h2 className="text-2xl font-semibold text-secondary"> AED {addCommasToNumber(attributes.Price)} <span className="text-sm font-medium "> / Per Year</span></h2>
-
-            <div className="flex my-4 ">
-              <div className="_center pr-5">
-                <LuBedDouble className="mr-2 stroke-primary stroke-[2px]" />
-                <span className="font-light text-primary">{12}</span>
-              </div>
-              <div className="_center border-l border-r border-[#00000010] px-5">
-                <LuBath className="mr-2 stroke-primary stroke-[2px] " />
-                <span className="font-light text-primary">{12}</span>
-              </div>
-              <div className="_center px-5">
-                <LuMaximize className="mr-2 stroke-primary stroke-[2px]" />
-                <span className="font-light text-primary">{12} sq.ft</span>
-              </div>
-            </div>
-
-            <p className="font-light mb-5">{attributes.Short_Address}</p>
-            <hr className="opacity-10" />
-            <h2 className="text-xl font-semibold text-secondary mt-5 mb-2.5">Description</h2>
-            <Formetter text={attributes.Description} />
-          </main>
-
-          <aside className="grid gap-5 mb-5 min-w-[320px]">
-            <ContactCard />
-            <LocationCard geopoint={attributes.Geopoints} />
-          </aside>
-        </section>
-      </div>
-    </Container>
+    <Template
+      {...{
+        type: "rent",
+        area: attributes.Area,
+        price: attributes.Price,
+        bed: attributes.Bedrooms,
+        parking: attributes.Parking,
+        bath: attributes.Bathrooms,
+        geopoint: attributes.Geopoints,
+        description: attributes.Description,
+        short_address: attributes.Short_Address,
+        images: [...attributes.Cron_Images.data.map((e: any) => e.url)],
+      }}
+    />
   );
 }
-
 // fetcher component
 const getProperty = async (slug: string) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/rent/${slug}`, {
---
+++ src/blocks/templates/detailed.tsx
@@ -0,0 +1,71 @@
+interface Props {
+    bed: number,
+    bath: number,
+    price: number,
+    parking: number,
+    images: string[],
+    geopoint: string,
+    description: string,
+    short_address: string,
+    area: number
+    type: 'buy' | 'rent' | 'offplan',
+}
+
+import React from "react";
+import Container from "@/blocks/atoms/container";
+import Images from "@/blocks/atoms/image-preview";
+import Formetter from "@/blocks/atoms/text-formatter";
+import ContactCard from "@/blocks/molecules/cards/contact";
+import addCommasToNumber from "@/app/lib/addCommasToNumbers";
+import LocationCard from "@/blocks/molecules/cards/location";
+import { LuBedDouble, LuBath, LuMaximize, LuParkingSquare } from "react-icons/lu";
+
+const PropertyDetailedPage: React.FC<Props> = ({
+    images, type, price, short_address, description, geopoint, bed, parking, bath, area
+}) => {
+    return (
+        <Container>
+            <div>
+                <Images media={images} />
+                <section className="grid lg:grid-cols-[3fr_1fr] gap-10 lg:px-0 px-5 ">
+                    <main>
+                        <h2 className="text-2xl font-semibold text-secondary"> AED {addCommasToNumber(price)} {type == 'rent' && <span className="text-sm uppercase text-primary " >/ per year</span>}</h2>
+
+                        <div className="flex my-4 ">
+                            <div className="_center pr-5">
+                                <LuBedDouble className="mr-2 stroke-primary stroke-[2px]" />
+                                <span className="font-light text-primary">{bed}</span>
+                            </div>
+                            <div className="_center border-l border-r border-[#00000010] px-5">
+                                <LuBath className="mr-2 stroke-primary stroke-[2px] " />
+                                <span className="font-light text-primary">{bath}</span>
+                            </div>
+                            <div className="_center border-l border-r border-[#00000010] px-5">
+                                <LuParkingSquare className="mr-2 stroke-primary stroke-[2px] " />
+                                <span className="font-light text-primary">{parking}</span>
+                            </div>
+                            <div className="_center px-5">
+                                <LuMaximize className="mr-2 stroke-primary stroke-[2px]" />
+                                <span className="font-light text-primary">{addCommasToNumber(area)} sq.ft</span>
+                            </div>
+                        </div>
+
+                        <p className="font-light mb-5">{short_address}</p>
+                        <hr className="opacity-10" />
+                        <h2 className="text-xl font-semibold text-secondary mt-5 mb-2.5">Description</h2>
+                        <Formetter text={description} />
+
+                    </main>
+
+                    <aside className="grid gap-5 mb-5 min-w-[320px]">
+                        <ContactCard />
+                        <LocationCard geopoint={geopoint} />
+                    </aside>
+                </section>
+            </div>
+        </Container>
+
+    )
+}
+
+export default PropertyDetailedPage
