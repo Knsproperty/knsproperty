d307ca48c167201b41c3e2d489d2a8a9e6084c15
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-15 13:31:59 +0530

    ✅ google map not-direct-integration, ✅ Text Formetting in property-detailed-page

---
+++ src/blocks/atoms/text-formatter.tsx
@@ -0,0 +1,10 @@
+interface Props {
+    text: string
+}
+const TextFormatter: React.FC<Props> = ({ text }) => {
+    const formattedText = text.replace(/(?:\r\n|\r|\n)/g, '<br/>')
+    return (
+        <p className="font-light text-primary" dangerouslySetInnerHTML={{ __html: formattedText }} />
+    )
+}
+export default TextFormatter
--- src/blocks/molecules/cards/location.tsx
+++ src/blocks/molecules/cards/location.tsx
@@ -1,25 +1,20 @@
+interface Props {
+    geopoint: string
+}
+
 import React from 'react'
 import { FaLocationDot } from 'react-icons/fa6'
 
-const LocationCard = () => {
+const LocationCard: React.FC<Props> = ({ geopoint }) => {
+    const href = `https://www.google.co.in/maps/place/${geopoint}`
     return (
-        <div className=' px-10 pb-8 pt-6 bg-[#E3ECEE] grid gap-5 rounded-md  bg-[url("/mapbg.webp")] bg-center bg-cover '>
-
+        <div className='px-10 pb-8 pt-6 bg-[#E3ECEE] grid gap-5 rounded-md  bg-[url("/mapbg.webp")] bg-center bg-cover '>
             <div>
-
-
                 <div className='_center py-10'>
                     <FaLocationDot color='blue' size={50} />
-
                 </div>
-
-                <button className=" w-full py-3.5 bg-opacity-50 border border-primary text-primary font-semibold text-sm rounded-full capitalize px-8 bg-[#E3ECEE]">Location Map</button>
-
+                <a target='_' href={href} className="w-full block text-center py-3.5 bg-opacity-50 border border-primary text-primary font-semibold text-sm rounded-full capitalize px-8 bg-[#E3ECEE]">Location Map</a>
             </div>
-
-
-
-
         </div>
     )
 }
