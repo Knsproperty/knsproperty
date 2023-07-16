ece16801bb01f68feaf08a98b8401f8a6fbf7b2e
dag-bag <virenderkumar23435@gmail.com>
2023-07-16 02:25:19 -0700

    Api Update

--- src/app/api/buy/[slug]/route.ts
+++ src/app/api/buy/[slug]/route.ts
@@ -2,11 +2,21 @@ import strapi, { populate } from "@/utils/strapi";
 import { NextResponse } from "next/server";
 export async function GET(req: Request, { params }: any) {
   const slug = params.slug;
-  const buy_properties = await strapi.find("buy-properties", {
+  const buy_properties = await strapi.find<any>("buy-properties", {
     populate,
     filters: {
       slug: slug,
     },
   });
-  return NextResponse.json(buy_properties.data);
+  let PropertyType = buy_properties.data[0].attributes.Property_Type;
+  const related_products = await strapi.find("buy-properties", {
+    populate,
+    filters: {
+      Property_Type: PropertyType,
+    },
+  });
+  return NextResponse.json({
+    data: buy_properties.data,
+    related_products: related_products,
+  });
 }
--- src/app/property/buy/[slug]/page.tsx
+++ src/app/property/buy/[slug]/page.tsx
@@ -1,28 +1,33 @@
 import Template from "@/blocks/templates/detailed";
 export default async function page({ params }: any) {
   const { slug } = params;
-  const [{ attributes }] = await getProperty(slug);
+  const {
+    data: [{ attributes, related_products }],
+  } = await getProperty(slug);
+  console.log(related_products);
   return (
-    <Template
-      {...{
-        type: "buy",
-        area: attributes.Area,
-        price: attributes.Price,
-        bed: attributes.Bedrooms,
-        parking: attributes.Parking,
-        bath: attributes.Bathrooms,
-        geopoint: attributes.Geopoints,
-        description: attributes.Description,
-        short_address: attributes.Short_Address,
-        images: [...attributes.Cron_Images.data.map((e: any) => e.url)],
-      }}
-    />
+    <>
+      <Template
+        {...{
+          type: "buy",
+          area: attributes.Area,
+          price: attributes.Price,
+          bed: attributes.Bedrooms,
+          parking: attributes.Parking,
+          bath: attributes.Bathrooms,
+          geopoint: attributes.Geopoints,
+          description: attributes.Description,
+          short_address: attributes.Short_Address,
+          images: [...attributes.Cron_Images.data.map((e: any) => e.url)],
+        }}
+      />
+    </>
   );
 }
 // fetcher component
 const getProperty = async (slug: string) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/buy/${slug}`, {
-    cache: "force-cache",
+    next: { revalidate: 60 },
   });
   return await res.json();
 };
