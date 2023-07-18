ee5ba920b4e8d9d915bb8116306f5eb19798e9ce
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-18 14:03:04 +0530

    testimonals added

---
+++ config/testimonals.ts
@@ -0,0 +1,20 @@
+export const testimonials = [
+    {
+        name: "Emily Davis",
+        image: "/149071.png",
+        position: "Marketing Manager",
+        review: "K&S Property sold my home quickly and exceeded expectations!"
+    },
+    {
+        name: "Michael Johnson",
+        image: "/149071.png",
+        position: "Freelance Designer",
+        review: "K&S Property found my perfect rental apartment hassle-free. Highly recommended!"
+    },
+    {
+        name: "Sarah Thompson",
+        image: "/149071.png",
+        position: "Real Estate Investor",
+        review: "K&S Property made my first investment purchase a success. Great expertise and support!"
+    },
+];
