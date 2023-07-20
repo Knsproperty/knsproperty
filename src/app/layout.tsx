872cf8d12f82c4d755259277ed9b70d537ce1857
dag-bag <virenderkumar23435@gmail.com>
2023-07-20 07:04:59 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Lexend } from "next/font/google";
 import "slick-carousel/slick/slick-theme.css";
 import { Header, Footer } from "@/blocks/layouts/main";
 import NewsLetterSection from "@/blocks/sections/newsletter/popup";
+import Script from "next/script";
+const GTM_ID = "GTM-MT9PWSWC";
 
 // font initlization
 const lexend = Lexend({ subsets: ["latin"] });
@@ -20,6 +22,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
