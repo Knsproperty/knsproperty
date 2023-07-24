2c0176f8bda6cefa628762095968e0c07987641c
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-24 10:11:02 +0530

    perfact about page

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,54 +1,52 @@
+'use client'
 import React from "react";
+import Link from "next/link";
 import { GiKey } from "react-icons/gi";
 import { BsHouseHeart } from "react-icons/bs";
+import Parallax from "@/blocks/atoms/parallex";
+import Service from "@/blocks/sections/services";
 import Container from "@/blocks/atoms/container";
 import Banner from "@/blocks/molecules/banner/banner";
-import Testimonals from "@/blocks/molecules/testimonals";
 import { PiSuitcaseSimpleThin } from "react-icons/pi";
-import Contact from "@/blocks/atoms/buttons/contact";
-import Service from "@/blocks/sections/services";
 
 const About = () => {
   return (
     <>
       <Banner>
-        <div className="grid grid-cols-1 text-center mt-10 ">
+        <div className="grid grid-cols-1 text-center mt-10">
           <h3 className="md:text-4xl text-3xl md:leading-normal leading-normal font-medium !text-white z-50">
             About Us
           </h3>
         </div>
       </Banner>
       <Container>
-
         <section className="relative lg:py-24 py-16">
           <div className="container">
             <div className="grid md:grid-cols-12 grid-cols-1 items-center gap-[30px]">
               <div className="md:col-span-5">
                 <div className="relative">
-                  <img src="/about.jpg" className="rounded-xl shadow-md" alt="" />
+                  <Parallax>
+                    <img src="/about.jpg" className="rounded-xl shadow-md" alt="image" />
+                  </Parallax>
                   <div className="absolute bottom-2/4 translate-y-2/4 start-0 end-0 text-center"></div>
                 </div>
               </div>
               {/*end col*/}
               <div className="md:col-span-7">
-                <div className="lg:ms-4">
-                  <h4 className="mb-6 md:text-3xl text-2xl lg:leading-normal leading-normal font-semibold">
-                    Efficiency. Transparency. <br /> Control.
-                  </h4>
+                <div className="lg:ms-4 px-5 lg:px-0">
+                  <h3 className="text-primary lg:text-4xl md:text-3xl text-2xl font-bold mb-10">
+                    Find the home of your dreams, <br />
+                    with us.
+                  </h3>
                   <p className="text-slate-400 max-w-xl">
-                    Hously developed a platform for the Real Estate marketplace
-                    that allows buyers and sellers to easily execute a transaction
-                    on their own. The platform drives efficiency, cost
-                    transparency and control into the hands of the consumers.
-                    Hously is Real Estate Redefined.
+                    The United Arab Emirates is a crossroads for individuals from all over the world.
+                    Housing and commercial properties in this futuristic city are profitable; therefore,
+                    investing in real estate in Dubai gives an opportunity to maximize profits.
                   </p>
                   <div className="mt-4">
-                    <a
-                      href=""
-                      className="btn bg-primary hover:bg-green-700 text-white rounded-md mt-3"
-                    >
-                      Learn More{" "}
-                    </a>
+                    <Link className="btn bg-primary hover:bg-green-700 text-white rounded-md mt-3" href={"/"}>
+                      Learn More
+                    </Link>
                   </div>
                 </div>
               </div>
@@ -59,13 +57,10 @@ const About = () => {
           {/*end container*/}
           <div className="container lg:mt-24 mt-16">
             <div className="grid grid-cols-1 pb-8 text-center">
-              <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
+              <h4 className="mb-6 md:text-3xl text-primary text-2xl lg:leading-normal leading-normal font-semibold">
                 How It Works
-              </h3>
-              <p className="text-slate-400 max-w-xl mx-auto">
-                A great plateform to buy, sell and rent your properties without
-                any agent or commisions.
-              </p>
+              </h4>
+              <p className="text-slate-400 max-w-xl mx-auto">K&amp;S is a great place to buy and rent properties.</p>
             </div>
             {/*end grid*/}
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 mt-8 gap-[30px]">
@@ -77,8 +72,6 @@ const About = () => {
                     width={24}
                     height={24}
                     viewBox="0 0 24 24"
-                    // fill="none"
-                    // stroke="currentColor"
                     strokeWidth={2}
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -91,10 +84,9 @@ const About = () => {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <h5 className="text-xl font-medium">Evaluate Property</h5>
+                  <h5 className="text-xl font-bold">Evaluate Property</h5>
                   <p className="text-slate-400 mt-3">
-                    If the distribution of letters and 'words' is random, the
-                    reader will not be distracted from making.
+                    Our expert team conducts thorough assessments of properties, from location and amenities to structural integrity, empowering you to make informed decisions.
                   </p>
                 </div>
               </div>
@@ -107,8 +99,6 @@ const About = () => {
                     width={24}
                     height={24}
                     viewBox="0 0 24 24"
-                    // fill="none"
-                    // stroke="currentColor"
                     strokeWidth={2}
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -121,10 +111,9 @@ const About = () => {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <h5 className="text-xl font-medium">Meeting with Agent</h5>
+                  <h5 className="text-xl font-bold">Meeting with Agent</h5>
                   <p className="text-slate-400 mt-3">
-                    If the distribution of letters and 'words' is random, the
-                    reader will not be distracted from making.
+                    Experience personalized service as our agents understand your needs, guiding you through property searches with tailored options.
                   </p>
                 </div>
               </div>
@@ -137,8 +126,6 @@ const About = () => {
                     width={24}
                     height={24}
                     viewBox="0 0 24 24"
-                    // fill="none"
-                    // stroke="currentColor"
                     strokeWidth={2}
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -151,10 +138,9 @@ const About = () => {
                   </div>
                 </div>
                 <div className="mt-6">
-                  <h5 className="text-xl font-medium">Close the Deal</h5>
+                  <h5 className="text-xl font-bold">Close the Deal</h5>
                   <p className="text-slate-400 mt-3">
-                    If the distribution of letters and 'words' is random, the
-                    reader will not be distracted from making.
+                    Our skilled negotiators work tirelessly to secure the best terms, ensuring a smooth and transparent closing process for your property investment.
                   </p>
                 </div>
               </div>
@@ -165,7 +151,6 @@ const About = () => {
           {/*end container*/}
         </section>
         <Award />
-        <Agents />
         <Service />
       </Container>
     </>
@@ -190,7 +175,7 @@ const Award = () => {
                   +
                 </h1>
                 <h5 className="counter-head text-white text-lg font-medium">
-                  Properties Sell
+                  Properties Sold
                 </h5>
               </div>
               {/*end counter box*/}
@@ -202,7 +187,7 @@ const Award = () => {
                   +
                 </h1>
                 <h5 className="counter-head text-white text-lg font-medium">
-                  Award Gained
+                  Awards Gained
                 </h5>
               </div>
               {/*end counter box*/}
@@ -224,135 +209,6 @@ const Award = () => {
         {/*end grid*/}
       </div>
       {/*end container*/}
-    </section>
-  );
-};
-
-const Agents = () => {
-  return (
-    <section className="relative md:pb-24 pb-16">
-      <div className="container lg:mt-24 my-16 ">
-        <div className="grid grid-cols-1 pb-8 text-center">
-          <h3 className="mb-4 md:text-3xl md:leading-normal text-2xl leading-normal font-semibold">
-            Meet The Agent Team
-          </h3>
-          <p className="text-slate-400 max-w-xl mx-auto">
-            A great plateform to buy, sell and rent your properties without any
-            agent or commisions.
-          </p>
-        </div>
-        <div className="grid md:grid-cols-12 grid-cols-1 mt-8 gap-[30px]">
-          <div className="lg:col-span-3 md:col-span-6">
-            <div className="group text-center">
-              <div className="relative inline-block mx-auto h-52 w-52 rounded-full overflow-hidden">
-                <img src="/04.35463172278c4051b5f4.jpg" className="" alt="" />
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-52 w-52 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out" />
-                <ul className="list-none absolute start-0 end-0 -bottom-20 group-hover:bottom-5 transition-all duration-500 ease-in-out ms-1 w-full h-full flex justify-center items-center">
-                  {/* <li className=" "> */}
-                  <Contact />
-                  {/* </li> */}
-                </ul>
-              </div>
-              <div className="content mt-3">
-                <a
-                  className="text-xl font-medium hover:text-green-600 transition-all duration-500 ease-in-out"
-                  href="/aboutus"
-                >
-                  Jack John
-                </a>
-                <p className="text-slate-400">Designer</p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:col-span-3 md:col-span-6">
-            <div className="group text-center">
-              <div className="relative inline-block mx-auto h-52 w-52 rounded-full overflow-hidden">
-                <img src="/05.a7ab2c82813cb95da9d6.jpg" className="" alt="" />
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-52 w-52 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out" />
-                <ul className="list-none absolute start-0 end-0 -bottom-20 group-hover:bottom-5 transition-all duration-500 ease-in-out ms-1 w-full h-full flex justify-center items-center">
-                  {/* <li className=" "> */}
-                  <Contact />
-                  {/* </li> */}
-                </ul>
-              </div>
-              <div className="content mt-3">
-                <a
-                  className="text-xl font-medium hover:text-green-600 transition-all duration-500 ease-in-out"
-                  href="/aboutus"
-                >
-                  Krista John
-                </a>
-                <p className="text-slate-400">Designer</p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:col-span-3 md:col-span-6">
-            <div className="group text-center">
-              <div className="relative inline-block mx-auto h-52 w-52 rounded-full overflow-hidden">
-                <img src="/06.7365b7fa25b4bccc5ff9.jpg" className="" alt="" />
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-52 w-52 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out" />
-                <ul className="list-none absolute start-0 end-0 -bottom-20 group-hover:bottom-5 transition-all duration-500 ease-in-out ms-1 w-full h-full flex justify-center items-center">
-                  {/* <li className=" "> */}
-                  <Contact />
-                  {/* </li> */}
-                </ul>
-              </div>
-              <div className="content mt-3">
-                <a
-                  className="text-xl font-medium hover:text-green-600 transition-all duration-500 ease-in-out"
-                  href="/aboutus"
-                >
-                  Roger Jackson
-                </a>
-                <p className="text-slate-400">Designer</p>
-              </div>
-            </div>
-          </div>
-          <div className="lg:col-span-3 md:col-span-6">
-            <div className="group text-center">
-              <div className="relative inline-block mx-auto h-52 w-52 rounded-full overflow-hidden">
-                <img src="/07.e3f0e9f968929876ad53.jpg" className="" alt="" />
-                <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black h-52 w-52 rounded-full opacity-0 group-hover:opacity-100 transition-all duration-500 ease-in-out" />
-                <ul className="list-none absolute start-0 end-0 -bottom-20 group-hover:bottom-5 transition-all duration-500 ease-in-out ms-1 w-full h-full flex justify-center items-center">
-                  {/* <li className=" "> */}
-                  <Contact />
-                  {/* </li> */}
-                </ul>
-              </div>
-              <div className="content mt-3">
-                <a
-                  className="text-xl font-medium hover:text-green-600 transition-all duration-500 ease-in-out"
-                  href="/aboutus"
-                >
-                  Johnny English
-                </a>
-                <p className="text-slate-400">Designer</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* End */}
-      <Testimonals />
-      <div className="container lg:mt-24 mt-16">
-        <div className="grid grid-cols-1 text-center">
-          <h3 className="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-medium text-black ">
-            Have Question ? Get in touch!
-          </h3>
-          <p className="text-slate-400 max-w-xl mx-auto">
-            A great plateform to buy, sell and rent your properties without any
-            agent or commisions.
-          </p>
-          <div className="mt-6">
-            <a
-              className="btn bg-primary hover:bg-green-700 text-white rounded-md"
-              href="/contact"
-            >
-              <i className="uil uil-phone align-middle me-2" /> Contact us
-            </a>
-          </div>
-        </div>
-      </div>
     </section>
   );
 };
