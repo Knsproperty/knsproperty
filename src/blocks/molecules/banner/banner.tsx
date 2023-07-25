2259cfbcb0abe011922dd6a5b421643c45fb795a
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-25 12:01:45 +0530

    updated

--- src/blocks/molecules/banner/banner.tsx
+++ src/blocks/molecules/banner/banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Banner({ children }: { children: React.ReactNode }) {
   return (
-    <section className="relative table w-full py-32 lg:py-36 bg-[url('/bg-1.webp')]  bg-no-repeat bg-center bg-cover">
+    <section className="relative table w-full py-32 lg:py-36 bg-[url('/code.jpg')]  bg-no-repeat bg-center bg-cover">
       <div className="absolute inset-0 bg-black opacity-60 -z-25" />
       <div className="container -z-26 max-w-[1320px] lg:mx-auto">
         {children}
