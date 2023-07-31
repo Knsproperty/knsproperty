5e7bcf59ed29f122d36c0e10319e3fc476d6917b
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-07-31 13:44:59 +0530

    community fixed

--- src/blocks/sections/community.tsx
+++ src/blocks/sections/community.tsx
@@ -19,71 +19,37 @@ export default function Community() {
           </div>
 
           <div className="grid grid-cols-1 gap-6 mt-8 sm:grid-cols-3 md:mt-16 lg:gap-x-12">
-            <Link href={"/property/buy"}>
-              <div className="relative group md:hover:-translate-y-10 ease-in-out duration-500 transition-all">
-                <div className="absolute top-0 left-0 w-full h-full _center bg-black bg-opacity-40 px-5 text-white hidden group-hover:flex ease-in-out duration-200 transition-all">
-                  <h1 className="text-white font-bold text-3xl translate-x-5 group-hover:-translate-x-5">
-                    DownTown Dubai
-                  </h1>
-                </div>
-                <Image
-                  width={370}
-                  height={530}
-                  className="w-full"
-                  src="/pic2.jpg"
-                  alt=""
-                />
-              </div>
-            </Link>
-
-            <Link href={"/property/buy"}>
-              <div className="relative group md:hover:-translate-y-10 ease-in-out duration-500 transition-all">
-                <div className="absolute top-0 left-0 w-full h-full _center bg-black bg-opacity-40 px-5 text-white hidden group-hover:flex ease-in-out duration-200 transition-all">
-                  <h1 className="text-white font-bold text-3xl  group-hover:translate-x-0">
-                    Dubai Marina
-                  </h1>
-                </div>
-                <Image
-                  width={370}
-                  height={530}
-                  className="w-full"
-                  src="/pic3.jpg"
-                  alt=""
-                />
-              </div>
-            </Link>
-
-            <Link href={"/property/buy"}>
-              <div className="relative group md:hover:-translate-y-10 ease-in-out duration-500 transition-all">
-                <div className="absolute top-0 left-0 w-full h-full _center bg-black bg-opacity-40 px-5 text-white hidden group-hover:flex ease-in-out duration-200 transition-all">
-                  <h1 className="text-white font-bold text-3xl translate-x-5 group-hover:-translate-x-5">
-                    Business Bay
-                  </h1>
-                </div>
-                <Image
-                  width={370}
-                  height={530}
-                  className="w-full"
-                  src="/pic4.jpg"
-                  alt=""
-                />
-              </div>
-            </Link>
-          </div>
-
-          <div className="mt-8 text-center md:mt-16">
-            <a
-              href="#"
-              title=""
-              className="inline-flex items-center justify-center py-4 font-semibold text-white transition-all duration-200 bg-blue-600 border border-transparent rounded-md px-14 hover:bg-blue-700 focus:bg-blue-700"
-              role="button"
-            >
-              {" "}
-              Join our team{" "}
-            </a>
+            <Comp
+              href="/property/buy"
+              title="DownTown Dubai"
+              image="/pic2.jpg"
+            />
+            <Comp href="/property/buy" title="Dubai Marina" image="/pic3.jpg" />
+            <Comp href="/property/buy" title="Business Bay" image="/pic4.jpg" />
           </div>
         </div>
       </section>
     </Container>
   );
 }
+
+const Comp = ({ href, image, title }: any) => {
+  return (
+    <Link href={href}>
+      <div className="relative h-full group md:hover:-translate-y-10 ease-in-out duration-500 transition-all">
+        <div className="absolute top-0 left-0 w-full h-full _center bg-black bg-opacity-40 px-5 text-white hidden group-hover:flex ease-in-out duration-200 transition-all">
+          <h1 className="text-white font-bold text-3xl translate-x-5 group-hover:-translate-x-5">
+            {title}
+          </h1>
+        </div>
+        <Image
+          width={370}
+          src={image}
+          alt={title}
+          height={530}
+          className="w-full h-full"
+        />
+      </div>
+    </Link>
+  );
+};
