80ebe02a88d68708dfc96b660c75b239d590d486
Deepak Vishwakarma <79501955+beliver-jsx@users.noreply.github.com>
2023-08-03 16:49:50 +0530

    aspect ratio

--- src/blocks/molecules/cards/team-card.tsx
+++ src/blocks/molecules/cards/team-card.tsx
@@ -1,29 +1,27 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 interface Props {
-    slug: string,
-    image: string,
-    name: string,
-    role: string,
+  slug: string;
+  image: string;
+  name: string;
+  role: string;
 }
 
 const TeamCard: React.FC<Props> = ({ slug, image, name, role }) => {
-    const url = `/about-us/team/${slug}`
-    return (
-        <Link href={url}>
-            <div className="grid group w-[300px] cursor-pointer">
-                <div className="aspect-w-5 aspect-h-6 overflow-hidden rounded-lg">
-                    <Image src={image} fill alt={name} />
-                </div>
+  const url = `/about-us/team/${slug}`;
+  return (
+    <Link href={url}>
+      <div className="grid group w-[300px] cursor-pointer">
+        <div className="aspect-w-5 aspect-h-5 overflow-hidden rounded-lg">
+          <Image src={image} fill alt={name} />
+        </div>
 
-                <div className="p-2">
-                    <h3 className="capitalize text-xl group-hover:underline">{name}</h3>
-                    <p className="capitalize text-sm">{role}</p>
-                </div>
-
-            </div>
-
-        </Link>
-    )
-}
-export default TeamCard
+        <div className="p-2">
+          <h3 className="capitalize text-xl group-hover:underline">{name}</h3>
+          <p className="capitalize text-sm">{role}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+export default TeamCard;
