2495f94a010d33461992d67c22cc7f754c4e7ec7
sohanemon <m.sohanemon@gmail.com>
2023-10-10 20:23:38 +0600

    rem: unnecessary filter

--- src/lib/slugBasedPosition.ts
+++ src/lib/slugBasedPosition.ts
@@ -1,19 +1,25 @@
-const slugBasesPosition = (teams : any) => {
-  return teams?.data?.map((_ : any, index: number) => {
-            const pos = index + 1;
-            return teams.data.filter(
-              (T: any) => parseInt(T.attributes.slug) == pos
-            )[0] ?? null;
-          })
-}
+const slugBasesPosition = (teams: any) => {
+  return teams?.data;
+  // return teams?.data?.map((_: any, index: number) => {
+  //   const pos = index + 1;
+  //   return (
+  //     teams.data.filter((T: any) => parseInt(T.attributes.slug) == pos)[0] ??
+  //     null
+  //   );
+  // });
+};
 
-export default slugBasesPosition
+export default slugBasesPosition;
 
-const differentiation = "Co-Founder / Managing Partner"
+const differentiation = 'Co-Founder / Managing Partner';
 export const filterLeaders = (profiles: any) => {
-  return profiles.filter((profile: any) => differentiation == profile.attributes.Position)
-}
+  return profiles.filter(
+    (profile: any) => differentiation == profile?.attributes?.Position
+  );
+};
 
 export const filterOthers = (profiles: any) => {
-  return profiles.filter((profile: any) =>differentiation !== profile.attributes.Position)
-}
+  return profiles.filter(
+    (profile: any) => differentiation !== profile?.attributes?.Position
+  );
+};
