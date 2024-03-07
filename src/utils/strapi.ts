85e5cb4acffbd6bf6ba4dd4f7e5c62bcaff14bad
aayanqazi <aayanqazi@yahoo.com>
2024-03-07 18:10:19 +0500

    Fixed bugs

--- src/utils/strapi.ts
+++ src/utils/strapi.ts
@@ -1,20 +1,20 @@
 /** @format */
 
-import Strapi from 'strapi-sdk-js';
+import Strapi from "strapi-sdk-js";
 
 const strapi = new Strapi({
-  url: 'https://ksn-strapi-production.up.railway.app',
-  prefix: '/api',
+  url: "https://ksn-strapi-production.up.railway.app",
+  prefix: "/api",
   store: {
-    key: 'strapi_jwt',
+    key: "strapi_jwt",
     useLocalStorage: false,
-    cookieOptions: { path: '/' },
+    cookieOptions: { path: "/" },
   },
   axiosOptions: {},
 });
 
-strapi.axios.defaults.headers.common['Authorization'] =
-  process.env.NEXT_PUBLIC_STRAPI_TOKEN || '';
-const populate = ['*', 'Preview_Image', 'Images', 'agent', 'agent.Profile'];
+strapi.axios.defaults.headers.common["Authorization"] =
+  process.env.NEXT_PUBLIC_STRAPI_TOKEN || "";
+const populate = ["*", "Preview_Image", "Images", "agent", "agent.Profile"];
 export default strapi;
 export { populate };
